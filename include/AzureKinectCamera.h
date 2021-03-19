1cbfc16beb60b00f9107b8185e543924b3fa1c93
Michael Peng <michael123.peng@gmail.com>
2021-03-19 15:18:25 -0700

    Incorporated CameraSetup class into AzureKinect

--- include/AzureKinectCamera.h
+++ include/AzureKinectCamera.h
@@ -2,9 +2,13 @@
 // OpenCV Libraries
 #include "Version.h"
 #include <opencv2/core.hpp>
+#include <librealsense2/rs.hpp>
+#include <thread>
+#include "concurrency.h"
+#include <atomic>
 
 // OpenARK Libraries
-#include "DepthCamera.h"
+#include "CameraSetup.h"
 
 namespace ark {
     /**
@@ -12,100 +16,104 @@ namespace ark {
     * Example on how to read from sensor and visualize its output
     * @include SensorIO.cpp
     */
-    class AzureKinectCamera : public DepthCamera
+    class AzureKinectCamera : public CameraSetup
     {
-    public:
+	public:
 
-        /**
-        * Public constructor initializing the Azure Kinect Camera.
-        * @param device_id camera device id. 0 is default.
-        * @param wide_fov_mode if true, starts Azure Kinect in wide FOV depth mode
-        * @param use_1080p if true, records in 1080p rather than 720p
-        * @param scale amount to scale down final image by
-        */
-        explicit AzureKinectCamera(uint32_t device_id = 0,
-                                   bool wide_fov_mode = false,
-                                   bool use_1080p = false,
-                                   double scale = 0.5);
+		/**
+		* Public constructor initializing the Azure Kinect Camera.
+		* @param device_id camera device id. 0 is default.
+		* @param wide_fov_mode if true, starts Azure Kinect in wide FOV depth mode
+		* @param use_1080p if true, records in 1080p rather than 720p
+		* @param scale amount to scale down final image by
+		*/
+		explicit AzureKinectCamera(uint32_t device_id = 0,
+			bool wide_fov_mode = false,
+			bool use_1080p = false,
+			double scale = 0.5);
 
-        /**
-        * Destructor for the Azure Kinect Camera.
-        */
-        ~AzureKinectCamera() override;
+		/**
+		* Destructor for the Azure Kinect Camera.
+		*/
+		~AzureKinectCamera() override;
+		/**
+		* Get the camera's model name.
+		*/
+		const std::string getModelName() const override;
 
-        /**
-         * Get the camera's model name.
-         */
-        const std::string getModelName() const override;
 
-        /** 
-         * Returns the width of the SR300 camera frame 
-         */
-        int getWidth() const override;
+		/**
+		* Returns the size of image
+		*/
+		cv::Size getImageSize() const override;
 
-        /** 
-         * Returns the height of the SR300 camera frame 
-         */
-        int getHeight() const override;
+		/**
+		* Sets the external hardware sync ans starts the camera
+		*/
+		void start() override;
 
-        /**
-         * Returns default detection parameters for this depth camera class
-         */
-        const DetectionParams::Ptr & getDefaultParams() const override;
+		void update(MultiCameraFrame & frame) override;
 
-        /**
-         * Returns true if an RGB image is available from this camera.
-         * @return true if an RGB image is available from this camera.
-         */
-        bool hasRGBMap() const override;
+		std::vector<float> getColorIntrinsics() override;
 
-        /** Preferred frame height */
-        const int PREFERRED_FRAME_H = 480;
+		void project(cv::Mat & xyz_map);
 
-        /** Shared pointer to Azure Kinect camera instance */
-        typedef std::shared_ptr<AzureKinectCamera> Ptr;
+		bool getImuToTime(double timestamp, std::vector<ImuPair>& data_out);
 
-        /** Get the timestamp of the last image in nanoseconds */
-        uint64_t getTimestamp();
+		void imuReader();
 
-        /** Get the basic calibration intrinsics
-         *  @return (fx, cx, fy, cy) */
-        cv::Vec4d getCalibIntrinsics();
+		/**Beyond this point are methods that are not part of CameraSetup
+		but and were originally part of the AzureKinectCamera class
 
-    protected:
-        /**
-        * Gets the new frame from the sensor (implements functionality).
-        * Updates xyzMap and ir_map.
-        */
-        void update(cv::Mat & xyz_map, cv::Mat & rgb_map, cv::Mat & ir_map, 
-                            cv::Mat & amp_map, cv::Mat & flag_map) override;
+		Function: Returns default detection parameters for this depth camera class:
+		const DetectionParams::Ptr & getDefaultParams() const override;
 
-        /**
-         * Initialize the camera, opening channels and resetting to initial configurations
-         */
-        void initCamera();
 
-        // internal storage
+		Function: Preferred frame height
+		const int PREFERRED_FRAME_H = 480;
 
-        double scale;
-        int width, height, scaled_width, scaled_height;
+		Function: Shared pointer to Azure Kinect camera instance
+		typedef std::shared_ptr<AzureKinectCamera> Ptr;
 
-        // Kinect Azure device (k4a_device_t)
-        void * k4a_device = NULL;
+		Function: Get the timestamp of the last image in nanoseconds
+		uint64_t getTimestamp();
 
-        // Kinect Azure depth/color transformation (k4a_transformation_t)
-        void * k4a_transformation = NULL;
+		Function: Get the basic calibration intrinsics @return (fx, cx, fy, cy)
+		cv::Vec4d getCalibIntrinsics();
+		*/
+	protected:
+		/**
+		* Gets the new frame from the sensor (implements functionality).
+		* Updates xyzMap and ir_map.
+		*/
 
-        // Cached XY position multiplier: multiply by current depth to get XY position (k4a_image_t *)
-        void * xy_table_cache = NULL;
+		/**
+		* Initialize the camera, opening channels and resetting to initial configurations
+		*/
+		//void initCamera();
 
-        const int32_t TIMEOUT_IN_MS = 1000;
+		// internal storage
 
-        mutable bool defaultParamsSet = false;
-        mutable DetectionParams::Ptr defaultParams;
+		double scale;
+		int width, height, scaled_width, scaled_height;
+		double last_ts_g;
 
-        int64_t timestamp;
-        /* (fx, cx, fy, cy) */
-        cv::Vec4d calib_intrin;
-    };
+		// Kinect Azure device (k4a_device_t)
+		void * k4a_device = NULL;
+
+		// Kinect Azure depth/color transformation (k4a_transformation_t)
+		void * k4a_transformation = NULL;
+
+		// Cached XY position multiplier: multiply by current depth to get XY position (k4a_image_t *)
+		void * xy_table_cache = NULL;
+
+		const int32_t TIMEOUT_IN_MS = 1000;
+
+		mutable bool defaultParamsSet = false;
+		mutable DetectionParams::Ptr defaultParams;
+
+		int64_t timestamp;
+		/* (fx, cx, fy, cy) */
+		cv::Vec4d calib_intrin;
+	};
 }
